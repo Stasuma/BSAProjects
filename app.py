e91ec0ace8e55ca20aebc616e050f765b11389e2
Stasuma <akorotkova310@gmail.com>
2024-02-25 14:41:18 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import requests 
+
+def getRequest():
+    response = requests.get("https://api.github.com/search/repositories")
+    st.write_stream(response)
+getRequest() 
